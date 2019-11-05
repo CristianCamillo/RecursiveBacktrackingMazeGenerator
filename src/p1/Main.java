f639bd618d344b0ae2a5cd2a5553fc91eafe97c8
CristianCamillo <56133559+CristianCamillo@users.noreply.github.com>
2019-11-05 23:03:29 +0100

    Add files via upload

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -1,5 +1,7 @@
 package p1;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
@@ -8,8 +10,8 @@ public class Main
 	private int width = 1000;
 	private int height = 1000;
 	
-	private int mWidth = 10;
-	private int mHeight = 10;
+	private int mWidth = 20;
+	private int mHeight = 20;
 	
 	private byte[] maze = new byte[mWidth * mHeight];	
 	private short[] currentCell = new short[2];
@@ -24,10 +26,11 @@ public class Main
 	private long lastGeneration = 0;
 	
 	private final static long GENERATE_NEW = 10000000l;
+	private final static Color DARK_GRAY = new Color(50, 50, 50);
 	
 	public Main()
 	{
-		SimpleEngineBuffered seb = new SimpleEngineBuffered(width, height, 1, "Recursive backtracking Maze Generator", 60, false, false)
+		FeatherEngine fe = new FeatherEngine(width, height, "Recursive Backtracking Maze Generator", 60, false, false)
 		{
 			public void update()
 			{
@@ -100,38 +103,37 @@ public class Main
 			
 			public void render()
 			{
-				RenderContext screen = getScreen();
-				screen.fillBackground();
+				Graphics g = getGraphics();
 				
-				screen.setColor((byte)0xff, (byte)0x22, (byte)0x22, (byte)0x22);
-				screen.fillRectangle(0, 0, width, height);						
+				g.setColor(DARK_GRAY);
+				g.fillRect(0, 0, width, height);					
 				
-				screen.setColor((byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff);
+				g.setColor(Color.WHITE);
 				for(short y = 0; y < mHeight; y++)
 					for(short x = 0; x < mWidth; x++)
 					{		
 						if(maze[x + y * mWidth] == 0)
 							continue;
 						
-						screen.fillRectangle(x * tileSize + tile10th, y * tileSize + tile10th, tileSize - tile5th, tileSize - tile5th);
+						g.fillRect(x * tileSize + tile10th, y * tileSize + tile10th, tileSize - tile5th, tileSize - tile5th);
 
 						if(x > 0 && maze[x-1 + y     * mWidth] == 3)
-							screen.fillRectangle(x * tileSize - tile10th, y * tileSize + tile10th, tile5th, tile5th * 4);
+							g.fillRect(x * tileSize - tile10th, y * tileSize + tile10th, tile5th, tile5th * 4);
 						
 						if(y > 0 && maze[x   + (y-1) * mWidth] == 4)
-							screen.fillRectangle(x * tileSize + tile10th, y * tileSize - tile10th, tile5th * 4, tile5th);
+							g.fillRect(x * tileSize + tile10th, y * tileSize - tile10th, tile5th * 4, tile5th);
 						
 						if(x < mWidth - 1 && maze[x+1 + y     * mWidth] == 1)
-							screen.fillRectangle((x+1) * tileSize - tile10th, y * tileSize + tile10th, tile5th, tile5th * 4);
+							g.fillRect((x+1) * tileSize - tile10th, y * tileSize + tile10th, tile5th, tile5th * 4);
 						
 						if(y < mHeight - 1 && maze[x   + (y+1) * mWidth] == 2)
-							screen.fillRectangle(x * tileSize + tile10th, (y+1) * tileSize - tile10th, tile5th * 4, tile5th);
+							g.fillRect(x * tileSize + tile10th, (y+1) * tileSize - tile10th, tile5th * 4, tile5th);
 					}
 				
 				if(currentCell[0] != -1)
 				{
-					screen.setColor((byte)0xff, (byte)0x00, (byte)0xff, (byte)0x00);
-					screen.fillRectangle(currentCell[0] * tileSize, currentCell[1] * tileSize, tileSize, tileSize);				
+					g.setColor(Color.GREEN);
+					g.fillRect(currentCell[0] * tileSize, currentCell[1] * tileSize, tileSize, tileSize);				
 				}
 			}
 			
@@ -143,14 +145,14 @@ public class Main
 		maze[currentCell[0] + currentCell[1] * mWidth] = -1;
 		
 		if(mWidth > mHeight)
-			tileSize = 1000 / mWidth;
+			tileSize = width / mWidth;
 		else
-			tileSize = 1000 / mHeight;
+			tileSize = width / mHeight;
 		
 		tile5th  = tileSize / 5;
 		tile10th = tileSize / 10;
 		
-		seb.start();
+		fe.start();
 	}
 	
 	public byte random(byte n)
